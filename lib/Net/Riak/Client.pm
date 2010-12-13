e277772928f4bdccbaae3acf5df5f3889e476a81
franck cuny <franck@lumberjaph.net>
2010-12-13 12:02:00 +0100

    move code from client to Role::REST

--- lib/Net/Riak/Client.pm
+++ lib/Net/Riak/Client.pm
@@ -1,66 +1,7 @@
 package Net::Riak::Client;
 
 use Moose;
-use MIME::Base64;
-use Moose::Util::TypeConstraints;
 
-class_type 'HTTP::Request';
-class_type 'HTTP::Response';
-
-has prefix => (
-    is      => 'rw',
-    isa     => 'Str',
-    default => 'riak'
-);
-has mapred_prefix => (
-    is      => 'rw',
-    isa     => 'Str',
-    default => 'mapred'
-);
-has [qw/r w dw/] => (
-    is      => 'rw',
-    isa     => 'Int',
-    default => 2
-);
-has client_id => (
-    is         => 'rw',
-    isa        => 'Str',
-    lazy_build => 1,
-);
-has http_request => (
-    is => 'rw',
-    isa => 'HTTP::Request',
-);
-
-has http_response => (
-    is => 'rw',
-    isa => 'HTTP::Response',
-    handles => ['is_success']
-);
-
-has ua_timeout => (
-    is  => 'rw',
-    isa => 'Int',
-    default => 3
-);
-
-with 'Net::Riak::Role::UserAgent';
-with qw/
-  Net::Riak::Role::REST
-  Net::Riak::Role::Hosts
-  /;
-
-
-
-sub _build_client_id {
-    "perl_net_riak" . encode_base64(int(rand(10737411824)), '');
-}
-
-sub is_alive {
-    my $self     = shift;
-    my $request  = $self->new_request('GET', ['ping']);
-    my $response = $self->send_request($request);
-    $self->is_success ? return 1 : return 0;
-}
+with 'MooseX::Traits';
 
 1;
--- lib/Net/Riak/Role/REST.pm
+++ lib/Net/Riak/Role/REST.pm
@@ -4,11 +4,50 @@ package Net::Riak::Role::REST;
 
 use URI;
 use HTTP::Request;
-use Moose::Role;
+use MIME::Base64;
 
-requires 'http_request';
-requires 'http_response';
-requires 'useragent';
+use Moose::Role;
+use Moose::Util::TypeConstraints;
+
+class_type 'HTTP::Request';
+class_type 'HTTP::Response';
+
+has prefix => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => 'riak'
+);
+has mapred_prefix => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => 'mapred'
+);
+has [qw/r w dw/] => (
+    is      => 'rw',
+    isa     => 'Int',
+    default => 2
+);
+has client_id => (
+    is         => 'rw',
+    isa        => 'Str',
+    lazy_build => 1,
+);
+has http_request => (
+    is => 'rw',
+    isa => 'HTTP::Request',
+);
+
+has http_response => (
+    is => 'rw',
+    isa => 'HTTP::Response',
+    handles => ['is_success']
+);
+
+has ua_timeout => (
+    is  => 'rw',
+    isa => 'Int',
+    default => 3
+);
 
 sub _build_path {
     my ($self, $path) = @_;
@@ -40,6 +79,17 @@ sub send_request {
     $self->http_response($r);
 
     return $r;
+}
+
+sub _build_client_id {
+    "perl_net_riak" . encode_base64(int(rand(10737411824)), '');
+}
+
+sub is_alive {
+    my $self     = shift;
+    my $request  = $self->new_request('GET', ['ping']);
+    my $response = $self->send_request($request);
+    $self->is_success ? return 1 : return 0;
 }
 
 1;
