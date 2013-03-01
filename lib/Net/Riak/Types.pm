21394d065f13ead860a8fcf9dc7a336f5e063f84
Robin Edwards <robin.ge@gmail.com>
2013-03-01 13:00:32 +0000

    Remove deprecations

--- lib/Net/Riak/Types.pm
+++ lib/Net/Riak/Types.pm
@@ -16,23 +16,4 @@ coerce RiakHost, from Str, via {
     [ { node => $_, weight => 1 } ];
 };
 
-coerce RiakHost, from ArrayRef, via {
-    warn "DEPRECATED: Support for multiple hosts will be removed in the 0.17 release.";
-    my $backends = $_;
-    my $weight   = 1 / @$backends;
-    [ map { { node => $_, weight => $weight } } @$backends ];
-};
-
-coerce RiakHost, from HashRef, via {
-    warn "DEPRECATED: Support for multiple hosts will be removed in the 0.17 release.";
-    my $backends = $_;
-    my $total    = 0;
-    $total += $_ for values %$backends;
-    [
-        map { { node => $_, weight => $backends->{$_} / $total } }
-          keys %$backends
-    ];
-};
-
 1;
-
